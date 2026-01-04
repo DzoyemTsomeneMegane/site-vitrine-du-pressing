ffda7d970eea94b87320167b96b031123d0efebb
DzoyemTsomeneMegane <116685063+DzoyemTsomeneMegane@users.noreply.github.com>
2026-01-04 13:16:37 +0100

    ajout2 commite

--- src/components/testimonialSection/TestimonialCard.jsx
+++ src/components/testimonialSection/TestimonialCard.jsx
@@ -5,7 +5,7 @@ const TestimonialCard = ({ quote, name, image }) => {
   return (
     <div className="testimonial-card">
       <div className="testimonial-image-container">
-        <img src={image} alt={`Photo de ${name}`} className="testimonial-image" />
+        <img src={image} alt={name} className="testimonial-image" />
       </div>
       <p className="testimonial-quote">"{quote}"</p>
       <p className="testimonial-name">- {name}</p>
